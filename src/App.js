001b7b76895f6c742c7278cd77b2c9d90174f1d2
jiannaso <jiannaso@gmail.com>
2025-05-12 17:16:50 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Sidebar from './components/sidebar';
 import Header from './components/header';
 import Playground from './components/playground';
-import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 
 function App() {
   return (
