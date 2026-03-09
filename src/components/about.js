3a8b8619169fa5fa3dc06c9d1729a34283545a56
jiannaso <jiannaso@gmail.com>
2026-03-09 10:59:59 -0400

    chi update

--- src/components/about.js
+++ src/components/about.js
@@ -17,7 +17,7 @@ export const About = () => {
     {
       id: -1,
       title: 'CHI 2026 | Beyond Anthropomorphism: a Spectrum of Interface Metaphors for LLMs',
-      name: '<b>TLDR</b>: the anthropomorphic metaphor has been overly reinforced in LLMs, so we look towards alternate metaphors that reveal LLMs as sociotechnical systems. \n\n Read the <a href=\'https://arxiv.org/abs/2603.04613\'>[arxiv preprint]</a> and please send me your thoughts!' ,
+      name: '<b>TLDR</b>: LLMs are sociotechnical systems, not human. However, the anthropomorphic metaphor has been overly reinforced in LLMs. In this, we propose alternate metaphors that . \n\n Read the <a href=\'https://arxiv.org/abs/2603.04613\'>[arxiv preprint]</a> and please send us your thoughts!' ,
       desc: 'March 2026 ',
       link: '',
       linkname: '',
